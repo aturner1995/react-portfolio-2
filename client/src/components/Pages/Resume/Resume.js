743f310dd2d9624a7ca8375f47741e221984dd95
Aaron <aaturner1995@gmail.com>
2023-06-01 16:58:03 -0300

    added skills

--- client/src/components/Pages/Resume/Resume.js
+++ client/src/components/Pages/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, ListGroup, Card } from 'react-bootstrap';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import data from '../../../data';
 
@@ -65,23 +65,83 @@ const EducationCard = ({ education }) => {
 const Resume = () => {
     return (
         <>
+            <Row className='about-section p-4 bg-light'>
+                <Col lg={3}>
+                    <h2 className='text-left'>SKILLS</h2>
+                    <a className=' btn prime-custom-2' href="resume-aaronturner-2023.pdf" target="_blank">Download Resume</a>
+                </Col>
+                <Col lg={9}>
+                    <Row>
+                        <Col className="d-flex justify-content-center my-2">
+                            <Card style={{ width: '18rem' }} className='text-center'>
+                                <Card.Body>
+                                    <Card.Title className='title'>LANGUAGES</Card.Title>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item action>JavaScript (ES6)</ListGroup.Item>
+                                        <ListGroup.Item action>Python</ListGroup.Item>
+                                        <ListGroup.Item action>CSS3</ListGroup.Item>
+                                        <ListGroup.Item action>HTML5</ListGroup.Item>
+                                        <ListGroup.Item action>SQL</ListGroup.Item>
+                                        <ListGroup.Item action>NoSQL</ListGroup.Item>
+                                        <ListGroup.Item action>MATLAB</ListGroup.Item>
+                                    </ListGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="d-flex justify-content-center my-2">
+                            <Card style={{ width: '18rem' }} className='text-center'>
+                                <Card.Body>
+                                    <Card.Title className='title'>TOOLS</Card.Title>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item action>jQuery</ListGroup.Item>
+                                        <ListGroup.Item action>ReactJS</ListGroup.Item>
+                                        <ListGroup.Item action>Vue.JS</ListGroup.Item>
+                                        <ListGroup.Item action>Bootstrap</ListGroup.Item>
+                                        <ListGroup.Item action>Node.JS</ListGroup.Item>
+                                        <ListGroup.Item action>Express.JS</ListGroup.Item>
+                                        <ListGroup.Item action>Bulma</ListGroup.Item>
+                                    </ListGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="d-flex justify-content-center my-2">
+                            <Card style={{ width: '18rem' }} className='text-center'>
+                                <Card.Body>
+                                    <Card.Title className='title'>APPLICATIONS</Card.Title>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item action>GitHub</ListGroup.Item>
+                                        <ListGroup.Item action>MySQL</ListGroup.Item>
+                                        <ListGroup.Item action>MongoDB</ListGroup.Item>
+                                        <ListGroup.Item action>Heroku</ListGroup.Item>
+                                        <ListGroup.Item action>SolidWorks</ListGroup.Item>
+                                        <ListGroup.Item action>ANSYS FLUENT</ListGroup.Item>
+                                        <ListGroup.Item action>MS PROJECT</ListGroup.Item>
+                                    </ListGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
             <Row className='p-4 bg-light'>
-            <Col lg={3}>
-                <h2 className='text-left'>EXPERIENCE</h2>
-                <a className=' btn prime-custom-2' href="resume-aaronturner-2023.pdf" target="_blank">Download Resume</a>
-            </Col>
-            <Col lg={9}>
-                <VerticalTimeline>
-                    {data.experience.map((experience, index) => (
-                        <ExperienceCard
-                            key={`experience-${index}`}
-                            experience={experience}
-                        />
-                    ))}
-                </VerticalTimeline>
-            </Col>
-        </Row>
-                <Row className='p-4 bg-light'>
+                <Col lg={3}>
+                    <h2 className='text-left'>EXPERIENCE</h2>
+                    <a className=' btn prime-custom-2' href="resume-aaronturner-2023.pdf" target="_blank">Download Resume</a>
+                </Col>
+                <Col lg={9}>
+                    <VerticalTimeline>
+                        {data.experience.map((experience, index) => (
+                            <ExperienceCard
+                                key={`experience-${index}`}
+                                experience={experience}
+                            />
+                        ))}
+                    </VerticalTimeline>
+                </Col>
+            </Row>
+            <Row className='p-4 bg-light'>
                 <Col lg={3}>
                     <h2 className='text-left'>EDUCATION</h2>
                     <a className=' btn prime-custom-2' href="resume-aaronturner-2023.pdf" target="_blank">Download Resume</a>
